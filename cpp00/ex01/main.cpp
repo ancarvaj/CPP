14aa94c0be6cc39d73835fe823ba39db879b3299
ancarvaj <44304145ac@gmail.com>
2024-12-09 17:12:58 +0100

    aa

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -12,7 +12,7 @@ std::string	get_contact_field(const std::string field)
 
 int	ft_add_contact(Contact *con)
 {
-	
+	std::cout << std::endl;
 	con->setName(get_contact_field("Name: "));
 	if (!con->getName().compare(""))
 		return (1);
@@ -28,19 +28,55 @@ int	ft_add_contact(Contact *con)
 	con->setDarkSecret(get_contact_field("Darkest secret: "));
 	if (!con->getDarkSecret().compare(""))
 		return (1);
+	std::cout << std::endl;
 	return (0);
+}
+
+void	display_contact(PhoneBook book, int index)
+{
+	std::cout << std::endl;
+	std::cout << book.getContact(index).getName() << std::endl;
+	std::cout << book.getContact(index).getLastName() << std::endl;
+	std::cout << book.getContact(index).getNickname() << std::endl;
+	std::cout << book.getContact(index).getPhoneNumber() << std::endl;
+	std::cout << book.getContact(index).getDarkSecret() << std::endl;
+	std::cout << std::endl;
 }
 
 void	cout_len(std::string str, int len)
 {
-	if (str.length() >= 10)
-		str = str.replace(9, 10, ".");
 	std::cout << std::setw(len) << std::right << str.substr(0, 10) << "|";
+}
+
+int	user_contact_interface(int i, PhoneBook book)
+{
+	std::cout << "\nWhich contact do you want to see?\nenter a number betwen 1 and 8\n";
+	std::cout << "if you dont want to see a specific contact enter the number 0\n\n";
+	std::cin >> i;
+	if (i == 0)
+	{
+		std::cout << "exitig...\n\n";
+		return 1;
+	}
+	while (i < 1 || i  > 8 || !book.getContact(i - 1).getName().compare(""))
+	{
+		std::cout << "Wrong contact, enter a new one\n";
+		std::cin >> i;
+		if (i == 0)
+		{
+			std::cout << "exitig...\n\n";
+			return 1;
+		}
+	}
+	display_contact(book, i - 1);
+	return (0);
 }
 
 void	ft_search_contact(PhoneBook book)
 {
 	int	i = -1;
+
+	std::cout << std::endl;
 	while (++i < 8 && book.getContact(i).getName().compare(""))
 	{
 		std::cout << std::setw(10) << std::right << i + 1 << "|";
@@ -49,6 +85,23 @@ void	ft_search_contact(PhoneBook book)
 		cout_len(book.getContact(i).getNickname(), 10);
 		std::cout << std::endl;
 	}
+	if (user_contact_interface(0, book))
+		return ;
+	std::string user;
+	while (user.compare("no"))
+	{
+		std::cout << "do you want to see a new contact? yes/no\n";
+		std::cin >> user;
+		std::cout << std::endl;
+		if (!user.compare("yes"))
+		{
+			if (user_contact_interface(0, book))
+				return ;
+		}
+		else
+			continue ;
+	}
+	std::cout << std::endl;
 }
 
 int	main(void)
@@ -71,14 +124,17 @@ int	main(void)
 				i++;
 			}
 			else
-				std::cout << "Empty field\n";
+				std::cout << "Empty field\n\n";
 		}
 		else if (!str.compare("SEARCH"))
 			ft_search_contact(book);
 		else if (!str.compare("EXIT"))
+		{
+			std::cout << "\nExiting program successfully\n\n";
 			break ;
+		}
 		else
-			std::cout << "COMMANDS: ADD, SEARCH, EXIT\n";
+			std::cout << "COMMANDS: ADD, SEARCH, EXIT\n\n";
 
 	}
 	return (0);
